ec24e56dc2647358609edaac8bf2a9964144acbd
Tom Andrews <tom.andrews@c5alliance.com>
2019-01-31 14:41:45 +0000

    Get bus data from api and convert to dart model

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bus Tracker',
+      title: 'Island Mapper',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(title: 'Jersey Bus Tracker'),
+      home: HomePage(title: 'Island Mapper'),
     );
   }
 }
@@ -29,9 +29,13 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   GoogleMapController mapController;
+  BusData _busData;
+  Set<String> _routes = new Set<String>();
 
   @override
   Widget build(BuildContext context) {
+    fetchPost(); //TODO: Put this on a timer?
+
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
@@ -48,13 +52,17 @@ class HomePageState extends State<HomePage> {
         ));
   }
 
-  Future<BusData> fetchPost() async {
+  void fetchPost() async {
     final response = await http.get(
         'https://sojbuslivetimespublic.azurewebsites.net/api/Values/GetMin?secondsAgo=3600');
-
     if (response.statusCode == 200) {
       BusData busData = BusData.fromJson(json.decode(response.body));
-      return busData;
+      _busData = busData;
+      // _routes.add(busData.minimumInfoUpdates[0].line);
+      // for (var i = 0; i < busData.minimumInfoUpdates.length; i++) {
+      //   _routes.add(busData.minimumInfoUpdates[i].line);
+      // }
+      // busData.minimumInfoUpdates.map((bus) => _routes.add(bus.line));
     } else {
       throw Exception('Failed to get bus data');
     }
@@ -66,32 +74,45 @@ class HomePageState extends State<HomePage> {
     });
   }
 
+  ListView _buildRouteSelector() {
+    // return ListView.builder(
+    //     padding: const EdgeInsets.all(16.0),
+    //     itemBuilder: /*1*/ (context, i) {
+    //       if (i.isOdd) return Divider(); /*2*/
+
+    //       final index = i ~/ 2; /*3*/
+    //       for (int i = 0; i < _busData.minimumInfoUpdates.length; i++) {
+    //         Widget route =
+    //             _buildRouteRow(_busData.minimumInfoUpdates[i].toString());
+    //         return route;
+    //       }
+    //     });
+    // List<ListTile> routeRows = _busData.minimumInfoUpdates.map((bus) => new ListTile(title: bus.toString(), trailing: new Icon(Icons.favorite)));
+
+    // List<ListTile> routeRows = _routes.map((bus) => new ListTile(title: Text(bus)));
+    List<String> list = ['one', 'two', 'three', 'four'];
+
+    List<Text> widgets = list.map((name) => new Text(name)).toList();
+
+    return new ListView(children: widgets);
+  }
+
+  Widget _buildRouteRow(String busNumber) {
+    return ListTile(
+      title: Text(busNumber, style: const TextStyle(fontSize: 18.0)),
+      trailing: new Icon(Icons.favorite),
+    );
+  }
+
   void _selectRoutes() {
     Navigator.of(context).push(
       new MaterialPageRoute<void>(
-        // Add 20 lines from here...
         builder: (BuildContext context) {
           return new Scaffold(
             appBar: new AppBar(
               title: const Text('Select Routes'),
             ),
-            body: new Center(
-              child: FutureBuilder<BusData>(
-                future: fetchPost(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    var text = snapshot.data.minimumInfoUpdates[0].toString();
-                    return Text(text);
-                  } else if (snapshot.hasError) {
-                    return Text("${snapshot.error}");
-                  }
-
-                  // // By default, show a loading spinner
-                  // return CircularProgressIndicator();
-                  return Text('hello');
-                },
-              ),
-            ),
+            body: _buildRouteSelector(),
           );
         },
       ),
@@ -99,119 +120,58 @@ class HomePageState extends State<HomePage> {
   }
 }
 
-// class MyHomePage extends StatefulWidget {
-//   MyHomePage({Key key, this.title}) : super(key: key);
-//   final String title;
-
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   Future<BusData> fetchPost() async {
-//     final response = await http.get(
-//         'https://sojbuslivetimespublic.azurewebsites.net/api/Values/GetMin?secondsAgo=3600');
-
-//     if (response.statusCode == 200) {
-//       BusData busData = BusData.fromJson(json.decode(response.body));
-//       return busData;
-//     } else {
-//       throw Exception('Failed to get bus data');
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: FutureBuilder<BusData>(
-//           future: fetchPost(),
-//           builder: (context, snapshot) {
-//             if (snapshot.hasData) {
-//               var text = snapshot.data.minimumInfoUpdates[0].toString();
-//               return Text(text);
-//             } else if (snapshot.hasError) {
-//               return Text("${snapshot.error}");
-//             }
-
-//             // // By default, show a loading spinner
-//             // return CircularProgressIndicator();
-//             return Text('hello');
-//           },
-//         ),
-//       ),
-
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
-
-// class FetchData {}
 
 class BusData {
-  final List<dynamic> minimumInfoUpdates;
+  final List<MinimumInfoUpdate> minimumInfoUpdates;
   final List<dynamic> stops;
 
   BusData({this.minimumInfoUpdates, this.stops});
 
   factory BusData.fromJson(Map<String, dynamic> json) {
+    var minimumInfoUpdatesFromJson = json['minimumInfoUpdates'] as List;
+
+    List<MinimumInfoUpdate> minimumInfoUpdatesList = minimumInfoUpdatesFromJson.map((i) => 
+    MinimumInfoUpdate.fromJson(i)).toList();
+
     return BusData(
-        minimumInfoUpdates: json['minimumInfoUpdates'], stops: json['stops']);
+        minimumInfoUpdates: minimumInfoUpdatesList, stops: json['stops']);
   }
 }
 
-// class MinimumInfoUpdate {
-//   final String bus;
-//   final String line;
-//   final String cat;
-//   final int lat;
-//   final int lon;
-//   final int bearing;
-//   final String direction;
-//   final String time;
-//   final int age;
 
-//   MinimumInfoUpdate(
-//       {this.bus,
-//       this.line,
-//       this.cat,
-//       this.lat,
-//       this.lon,
-//       this.bearing,
-//       this.direction,
-//       this.time,
-//       this.age});
+class MinimumInfoUpdate {
+  final String bus;
+  final String line;
+  final String cat;
+  final double lat;
+  final double lon;
+  final int bearing;
+  final String direction;
+  final String time;
+  final int age;
 
-//   // factory MinimumInfoUpdate.fromJson(Map<String, dynamic> json) {
-//   //   return MinimumInfoUpdate(
-//   //     bus: json['bus'],
-//   //     line: json['line'],
-//   //     cat: json['cat'],
-//   //     lat: json['lat'],
-//   //     lon: json['lon'],
-//   //     bearing: json['bearing'],
-//   //     direction: json['direction'],
-//   //     time: json['time'],
-//   //     age: json['age'],
-//   //   );
-//   // }
-// }
+  MinimumInfoUpdate(
+      {this.bus,
+      this.line,
+      this.cat,
+      this.lat,
+      this.lon,
+      this.bearing,
+      this.direction,
+      this.time,
+      this.age});
+
+  factory MinimumInfoUpdate.fromJson(Map<String, dynamic> json) {
+    return MinimumInfoUpdate(
+      bus: json['bus'],
+      line: json['line'],
+      cat: json['cat'],
+      lat: json['lat'],
+      lon: json['lon'],
+      bearing: json['bearing'],
+      direction: json['direction'],
+      time: json['time'],
+      age: json['age'],
+    );
+  }
+}
