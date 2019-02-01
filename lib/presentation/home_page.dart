8debaab0871159086383fc6588a88c77906af6a1
Tom Andrews <tom.andrews@c5alliance.com>
2019-02-01 18:31:04 +0000

    Draw buses on map as markers

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'dart:convert';
 import 'package:bus_tracker/models/bus_data.dart';
 import 'package:bus_tracker/presentation/select_routes_page.dart';
@@ -9,6 +10,7 @@ class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
 
   final String title;
+
   @override
   State createState() => HomePageState();
 }
@@ -16,10 +18,18 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> {
   GoogleMapController mapController;
   BusData _busData;
+  HashMap<String, Marker> _busMarkers = new HashMap<String, Marker>();
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchBusData(); //TODO: Put this on a timer?
+  }
 
   @override
   Widget build(BuildContext context) {
-    fetchBusData(); //TODO: Put this on a timer?
+    _fetchBusData(); //TODO: Put this on a timer?
+    _updateBusMarkersOnMap();
 
     return Scaffold(
         appBar: AppBar(
@@ -38,15 +48,19 @@ class HomePageState extends State<HomePage> {
         body: Center(
           child: GoogleMap(
             onMapCreated: _onMapCreated,
-            initialCameraPosition: CameraPosition(target: LatLng(49.218360, -2.139824), zoom: 11),
-            cameraTargetBounds: CameraTargetBounds(LatLngBounds(southwest: LatLng(49.11, -2.25), northeast: LatLng(49.31, -2.00))),
+            initialCameraPosition:
+                CameraPosition(target: LatLng(49.218360, -2.139824), zoom: 11),
+            cameraTargetBounds: CameraTargetBounds(LatLngBounds(
+                southwest: LatLng(49.11, -2.25),
+                northeast: LatLng(49.31, -2.00))),
             minMaxZoomPreference: MinMaxZoomPreference(11, null),
             myLocationEnabled: true,
+            compassEnabled: true,
           ),
         ));
   }
 
-  void fetchBusData() async {
+  void _fetchBusData() async {
     final response = await http.get(
         'https://sojbuslivetimespublic.azurewebsites.net/api/Values/GetMin?secondsAgo=3600');
     if (response.statusCode == 200) {
@@ -57,10 +71,38 @@ class HomePageState extends State<HomePage> {
     }
   }
 
-  // map stuff
   void _onMapCreated(GoogleMapController controller) {
     setState(() {
       mapController = controller;
     });
+  }
+
+  Future _updateBusMarkersOnMap() async {
+    if (_busData != null) {
+      for (int i = 0; i < _busData.minimumInfoUpdates.length; i++) {
+        MarkerOptions markerOptions = MarkerOptions(
+            position: LatLng(_busData.minimumInfoUpdates[i].lat,
+                _busData.minimumInfoUpdates[i].lon),
+            infoWindowText: InfoWindowText(_busData.minimumInfoUpdates[i].line,
+                _busData.minimumInfoUpdates[i].bus));
+
+        if (_busMarkers.containsKey(_busData.minimumInfoUpdates[i].bus)) {
+          await mapController.updateMarker(
+              _busMarkers[_busData.minimumInfoUpdates[i].bus], markerOptions);
+        } else {
+          Marker marker = await mapController.addMarker(markerOptions);
+          _busMarkers[_busData.minimumInfoUpdates[i].bus] = marker;
+        }
+      }
+    }
+
+    // for (int i = 0; i < _busData.minimumInfoUpdates.length; i++) {
+    //   MarkerOptions markerOptions = MarkerOptions(
+    //       position: LatLng(_busData.minimumInfoUpdates[i].lat,
+    //           _busData.minimumInfoUpdates[i].lon));
+
+    //   Marker marker = await mapController.addMarker(markerOptions);
+    //   _busMarkers[_busData.minimumInfoUpdates[i].bus] = marker;
+    // }
   }
 }
