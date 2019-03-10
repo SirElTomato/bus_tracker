2707015976c79ec8641902d3c7c368153723e455
Tom Andrews <tom_a32@hotmail.co.uk>
2019-03-10 22:35:29 +0000

    Add styling to select routes page

--- lib/presentation/pages/select_routes_page.dart
+++ lib/presentation/pages/select_routes_page.dart
@@ -11,13 +11,14 @@ class SelectRoutesPage extends StatefulWidget {
 
 class _SelectRoutesPageState extends State<SelectRoutesPage> {
   final _routeDataBloc = kiwi.Container().resolve<RouteDataBloc>();
-  static const allTileWidth = 0.25;
-  static const tileWidth = 0.25;
+  static const allTileWidth = 0.563;
+  static const genericTileWidth = 0.25;
   static const tileHeight = 0.25;
   static const edgeLeft = 0.063;
   static const edgeTop = 0.05;
   static const edgeRight = 0.00;
   static const edgeBottom = 0.01;
+  double screenWidth;
 
   @override
   Widget build(BuildContext context) {
@@ -26,35 +27,53 @@ class _SelectRoutesPageState extends State<SelectRoutesPage> {
         title: Text('Select Routes'),
       ),
       body: StreamBuilder<List<BusRoute>>(
-        stream: _routeDataBloc.routeData,
-        initialData: List<BusRoute>(),
-        builder: (context, snapshot) => SingleChildScrollView(
-              child: Wrap(
-                children: snapshot.data
-                    .map((route) => _createRouteTile(route))
-                    .toList(),
-              ),
-            ),
+          stream: _routeDataBloc.routeData,
+          initialData: List<BusRoute>(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              screenWidth = MediaQuery.of(context).size.width;
+              return _buildRouteSelector(snapshot.data);
+            }
+          }),
+    );
+  }
+
+  SingleChildScrollView _buildRouteSelector(List<BusRoute> data) {
+    List<GridTile> routeTiles = data
+        .map((route) =>
+            (_createRouteTile(route.Number, route.Colour, genericTileWidth)))
+        .toList();
+
+    routeTiles.insert(0, _createRouteTile("All", "#b70005", allTileWidth));
+
+    return SingleChildScrollView(
+      child: Wrap(
+        children: routeTiles,
       ),
     );
   }
 
-  GridTile _createRouteTile(BusRoute busRoute) {
+  GridTile _createRouteTile(
+      String routeNumber, String colorInHex, double tileWidth) {
     return GridTile(
       child: Opacity(
         opacity: 1,
         child: Container(
           child: Center(
             child: Text(
-              busRoute.Name,
+              routeNumber,
               style: TextStyle(color: Colors.white, fontSize: 24),
             ),
           ),
-          width: tileWidth,
-          height: tileHeight,
-          margin: EdgeInsets.fromLTRB(edgeLeft, edgeTop, edgeRight, edgeBottom),
+          width: tileWidth * screenWidth,
+          height: tileHeight * screenWidth,
+          margin: EdgeInsets.fromLTRB(
+              edgeLeft * screenWidth,
+              edgeTop * screenWidth,
+              edgeRight * screenWidth,
+              edgeBottom * screenWidth),
           decoration: BoxDecoration(
-            color: HexColor(busRoute.Colour),
+            color: HexColor(colorInHex),
             borderRadius: BorderRadius.all(const Radius.circular(50)),
           ),
         ),
