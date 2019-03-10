c5408ffb42be8cd2732ff4da2c4306394c6cbe71
Tom Andrews <tom_a32@hotmail.co.uk>
2019-03-10 21:25:37 +0000

    Extract google map to widget

--- lib/presentation/pages/map_page.dart
+++ lib/presentation/pages/map_page.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:track_my_travel/blocs/bus_data/bus_data_bloc.dart';
-import 'package:track_my_travel/data/models/bus_data/bus_data.dart';
-import 'package:track_my_travel/data/models/bus_data/minimum_info_update.dart';
 import 'package:track_my_travel/presentation/widgets/map_page_options_widget.dart';
 import 'package:track_my_travel/presentation/widgets/map_widget.dart';
 import 'package:kiwi/kiwi.dart' as kiwi;
@@ -13,8 +10,6 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   final _busDataBloc = kiwi.Container().resolve<BusDataBloc>();
-  GoogleMapController _mapController;
-  Set<Marker> _markers;
 
   @override
   Widget build(BuildContext context) {
@@ -27,47 +22,7 @@ class _MapPageState extends State<MapPage> {
       ),
       // drawer: Drawer(),
       floatingActionButton: MapPageOptionsWidget(),
-      body: StreamBuilder<List<MinimumInfoUpdate>>(
-          stream: _busDataBloc.busData,
-          initialData: List<MinimumInfoUpdate>(),
-          builder: (context, snapshot) {
-            return Center(
-              child: GoogleMap(
-                onMapCreated: (GoogleMapController controller) {
-                  _mapController = controller;
-                },
-                initialCameraPosition: CameraPosition(
-                    target: LatLng(49.218360, -2.139824), zoom: 11),
-                cameraTargetBounds: CameraTargetBounds(
-                  LatLngBounds(
-                    southwest: LatLng(49.11, -2.25),
-                    northeast: LatLng(49.31, -2.00),
-                  ),
-                ),
-                minMaxZoomPreference: MinMaxZoomPreference(11, null),
-                myLocationEnabled: true,
-                compassEnabled: true,
-                rotateGesturesEnabled: true,
-                tiltGesturesEnabled: true,
-                markers:
-                    snapshot.data.map((update) => buildMarker(update)).toSet(),
-              ),
-            );
-          }),
+      body: MapWidget(_busDataBloc),
     );
-  }
-
-  Marker buildMarker(MinimumInfoUpdate update) {
-    return Marker(
-      markerId: MarkerId(update.bus + update.direction),
-      position: LatLng(update.lat, update.lon),
-      infoWindow: InfoWindow(title: update.line, snippet: update.direction),
-      rotation: update.bearing.toDouble(),
-      anchor: Offset(0.5, 0.5),
-    );
-  }
-
-  ListTile buildTile(String name) {
-    return ListTile(title: Text(name));
   }
 }
--- lib/presentation/widgets/map_widget.dart
+++ lib/presentation/widgets/map_widget.dart
@@ -1,35 +1,58 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:track_my_travel/blocs/bus_data/bus_data_bloc.dart';
+import 'package:track_my_travel/data/models/bus_data/minimum_info_update.dart';
 
 class MapWidget extends StatefulWidget {
+  final BusDataBloc busDataBloc;
+
+  MapWidget(this.busDataBloc);
+
   @override
   _MapWidgetState createState() => _MapWidgetState();
 }
 
 class _MapWidgetState extends State<MapWidget> {
-  Completer<GoogleMapController> _mapController = Completer();
+  GoogleMapController _mapController;
 
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      onMapCreated: (GoogleMapController controller) {
-        _mapController.complete(controller);
+    return StreamBuilder<List<MinimumInfoUpdate>>(
+      stream: widget.busDataBloc.busData,
+      initialData: List<MinimumInfoUpdate>(),
+      builder: (context, snapshot) {
+        return Center(
+          child: GoogleMap(
+            onMapCreated: (GoogleMapController controller) {
+              _mapController = controller;
+            },
+            initialCameraPosition:
+                CameraPosition(target: LatLng(49.218360, -2.139824), zoom: 11),
+            cameraTargetBounds: CameraTargetBounds(
+              LatLngBounds(
+                southwest: LatLng(49.11, -2.25),
+                northeast: LatLng(49.31, -2.00),
+              ),
+            ),
+            minMaxZoomPreference: MinMaxZoomPreference(11, null),
+            myLocationEnabled: true,
+            compassEnabled: true,
+            rotateGesturesEnabled: true,
+            tiltGesturesEnabled: true,
+            markers: snapshot.data.map((update) => buildMarker(update)).toSet(),
+          ),
+        );
       },
-      initialCameraPosition:
-          CameraPosition(target: LatLng(49.218360, -2.139824), zoom: 11),
-      cameraTargetBounds: CameraTargetBounds(
-        LatLngBounds(
-          southwest: LatLng(49.11, -2.25),
-          northeast: LatLng(49.31, -2.00),
-        ),
-      ),
-      minMaxZoomPreference: MinMaxZoomPreference(11, null),
-      myLocationEnabled: true,
-      compassEnabled: true,
-      rotateGesturesEnabled: true,
-      tiltGesturesEnabled: true,
+    );
+  }
+
+  Marker buildMarker(MinimumInfoUpdate update) {
+    return Marker(
+      markerId: MarkerId(update.bus + update.direction),
+      position: LatLng(update.lat, update.lon),
+      infoWindow: InfoWindow(title: update.line, snippet: update.direction),
+      rotation: update.bearing.toDouble(),
+      anchor: Offset(0.5, 0.5),
     );
   }
 }
