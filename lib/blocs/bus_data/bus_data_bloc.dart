e5689bcc127f3b75734c8f928f9c6f13b0d2bfa7
Tom Andrews <tom_a32@hotmail.co.uk>
2019-03-10 21:10:56 +0000

    Populate stream of bus data periodically

--- lib/blocs/bus_data/bus_data_bloc.dart
+++ lib/blocs/bus_data/bus_data_bloc.dart
@@ -1,33 +1,40 @@
 import 'dart:async';
 
 import 'package:track_my_travel/data/models/bus_data/bus_data.dart';
+import 'package:track_my_travel/data/models/bus_data/minimum_info_update.dart';
 import 'package:track_my_travel/data/respository/bus_data_repository.dart';
 import 'package:rxdart/rxdart.dart';
 
 class BusDataBloc {
   BusDataRepository _busDataRepository;
+  Timer timer;
 
-  final _busDataSubject = BehaviorSubject<BusData>();
+  List<String> selectedRoutes;
 
-  Stream<BusData> get busData => _busDataSubject.stream;
+  final _busDataSubject = BehaviorSubject<List<MinimumInfoUpdate>>();
+
+  Stream<List<MinimumInfoUpdate>> get busData => _busDataSubject.stream;
 
   var _busData = BusData();
 
-  BusDataBloc(BusDataRepository busDataRepository) {
-    this._busDataRepository = busDataRepository;
-
+  BusDataBloc(BusDataRepository busDataRepository)
+      : _busDataRepository = busDataRepository,
+        selectedRoutes = ['1', '15', '22'] {
     _getBusData();
-    startUpdatingBusData();
-  }
 
-  void startUpdatingBusData() {
-    const oneSec = const Duration(milliseconds: 1000);
-    new Timer.periodic(oneSec, (Timer t) => _getBusData());
+    timer = Timer.periodic(
+      Duration(seconds: 1),
+      (Timer t) => _getBusData(),
+    );
   }
 
   Future _getBusData() async {
+    print('getting bus data');
     _busData = await _busDataRepository.getBusData();
-    _busDataSubject.add(_busData);
+    List<MinimumInfoUpdate> filteredBusData = _busData.minimumInfoUpdates
+        .where((update) => selectedRoutes.contains(update.line))
+        .toList();
+    _busDataSubject.add(filteredBusData);
   }
 
   void close() {
--- lib/presentation/pages/map_page.dart
+++ lib/presentation/pages/map_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:track_my_travel/blocs/bus_data/bus_data_bloc.dart';
 import 'package:track_my_travel/data/models/bus_data/bus_data.dart';
+import 'package:track_my_travel/data/models/bus_data/minimum_info_update.dart';
 import 'package:track_my_travel/presentation/widgets/map_page_options_widget.dart';
 import 'package:track_my_travel/presentation/widgets/map_widget.dart';
 import 'package:kiwi/kiwi.dart' as kiwi;
@@ -15,34 +16,28 @@ class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: Colors.transparent,
-      //   title: Text(
-      //     'Track my Travel',
-      //   ),
-      // ),
-      drawer: Drawer(),
-      floatingActionButton: MapPageOptionsWidget(),
-      body: StreamBuilder<BusData>(
+        // appBar: AppBar(
+        //   backgroundColor: Colors.transparent,
+        //   title: Text(
+        //     'Track my Travel',
+        //   ),
+        // ),
+        drawer: Drawer(),
+        floatingActionButton: MapPageOptionsWidget(),
+        body: StreamBuilder<List<MinimumInfoUpdate>>(
           stream: _busDataBloc.busData,
-          initialData: BusData(),
-          builder: (BuildContext context, snapshot) {
-            if (snapshot.data.minimumInfoUpdates.isNotEmpty) {
-              return Center(
-                child: MapWidget(),
-              );
-              return ListView(
-                children: snapshot.data.minimumInfoUpdates
-                    .map((update) => buildTile(update.lat.toString()))
+          initialData: List<MinimumInfoUpdate>(),
+          builder: (context, snapshot) => ListView(
+                children: snapshot.data
+                    .map((update) => buildTile(update.time))
                     .toList(),
-              );
-            } else {
-              return Center(
-                child: Text('no data'),
-              );
-            }
-          }),
-    );
+              ),
+        )
+
+        // Center(
+        //   child: MapWidget(_busDataBloc),
+        // ),
+        );
   }
 
   ListTile buildTile(String name) {
