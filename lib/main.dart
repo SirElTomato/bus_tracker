09f8de221eaea3c85b7449246742709f65561858
Tom Andrews <tom_a32@hotmail.co.uk>
2019-03-12 19:55:52 +0000

    Add requesting of location permission

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:track_my_travel/blocs/preferences/preferences_bloc.dart';
 import 'package:track_my_travel/injection_container.dart';
 import 'package:track_my_travel/presentation/pages/map_page.dart';
+import 'package:permission/permission.dart';
 
 void main() {
   initKiwi();
+  checkPermissions();
   runApp(MyApp());
 }
 
@@ -22,5 +24,29 @@ class MyApp extends StatelessWidget {
         preferencesBloc: preferencesBloc,
       ),
     );
+  }
+
+  // Future checkPermissions() async {
+  //   PermissionStatus locationPermission = await PermissionHandler()
+  //       .checkPermissionStatus(PermissionGroup.location);
+
+  //   if (locationPermission.value != PermissionStatus.granted.value) {
+  //     Map<PermissionGroup, PermissionStatus> permissions =
+  //         await PermissionHandler()
+  //             .requestPermissions([PermissionGroup.location]);
+  //   }
+  // }
+
+}
+
+Future checkPermissions() async {
+  List<Permissions> permissionStatuses =
+      await Permission.getPermissionsStatus([PermissionName.Location]);
+
+  for (int i = 0; i < permissionStatuses.length; i++) {
+    if (permissionStatuses[i].permissionStatus != PermissionStatus.allow) {
+      await Permission.requestPermissions(
+          [permissionStatuses[i].permissionName]);
+    }
   }
 }
